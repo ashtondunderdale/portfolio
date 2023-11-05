66952d7f248918ce8ded7a645561b9324d47ed42
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-11-05 23:27:01 +0000

    added projects

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const backgroundColors = ["#FFEBD7", "#D7E4F3"];
+    const backgroundColors = ["#FFEBD7", "#D7E4F3", "#E4D7F3"];
 
     function setRandomBackgroundColor() {
         const randomIndex = Math.floor(Math.random() * backgroundColors.length);
